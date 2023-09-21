79228402d8c9200610eadd09426eda2e1012f822
Elisei <elisey-l@mail.ru>
2023-09-21 22:04:11 +0300

    01

---
+++ task19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите пятезначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10000;
+int c = a % 10;
+int x1 = a / 1000;
+int x2 = x1 % 10;
+int y1 = a / 10;
+int y2 = y1 % 10;
+if(b == c && x2 == y2)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
+
