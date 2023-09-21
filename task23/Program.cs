6743fe4616a2a8e7ef36f4103d913fbb180bfcf2
Elisei <elisey-l@mail.ru>
2023-09-21 22:05:03 +0300

    03

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿void CubeNum(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        Console.WriteLine($"{count} -> {count*count*count}");
+        count++;
+    }
+}
+
+Console.WriteLine("Введите число для нахождения куба числа:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CubeNum(number);
